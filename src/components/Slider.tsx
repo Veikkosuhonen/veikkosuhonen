8ed89f1f6c2d66e9bca8eabb8d067545dd25b3f6
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-02-25 02:16:00 +0200

    WHat

---
+++ src/components/Slider.tsx
@@ -0,0 +1,91 @@
+import { Component, For, createEffect, createSignal, onMount } from "solid-js"
+import { Setting } from "../graphics/settingsStore"
+import cn from "classnames"
+import { createPositionToElement, useMousePosition } from "@solid-primitives/mouse";
+
+const [dragStart, setDragStart] = createSignal<number>(0)
+
+const Slider: Component<{
+  setting: Setting,
+  disabled: boolean,
+  onChange: (s: Setting, v: number) => {}
+}> = (props) => {
+  const initialOffset = 100 - (props.setting.value - props.setting.min) / (props.setting.max - props.setting.min) * 100
+  const [offset, setOffset] = createSignal<number>(initialOffset)
+  const [previousOffset, setPreviousOffset] = createSignal<number>(initialOffset)
+  const [isDragging, setIsDragging] = createSignal<boolean>(false)
+  const [isHover, setIsHover] = createSignal<boolean>(false)
+  
+  onMount(() => {
+    addEventListener("mouseup", () => {
+      setIsDragging(false)
+      setPreviousOffset(offset())
+    })
+  })
+
+  const pos = useMousePosition()
+
+  const dragUpdate = () => {
+    let newOffset = previousOffset() + (pos.y - dragStart())
+    newOffset = Math.min(Math.max(newOffset, 0), 100)
+    setOffset(newOffset)
+
+    const newValue = ((100 - newOffset) / 100) * (props.setting.max - props.setting.min) + props.setting.min
+
+    props.onChange(props.setting, newValue)
+  }
+
+  createEffect(() => {
+    if (isDragging()) {
+      dragUpdate()
+    }
+  })
+
+  const onMouseDown = () => {
+    setDragStart(pos.y)
+    setIsDragging(true)
+    dragUpdate()
+  }
+
+  const ticks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  return (
+    <div class="w-16 flex flex-col items-center mb-2" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} draggable={false}>
+      <div class="relative flex justify-center h-[140px] w-10 overflow-y-hidden overflow-x-hidden cursor-pointer" onMouseDown={onMouseDown} draggable={false}>
+
+        <div class={`absolute flex flex-col items-center gap-[9px] -z-10 justify-self-start py-[14px] w-7
+          [&>*:nth-child(even)]:w-7
+          [&>*:nth-child(odd)]:w-5
+        `}>
+          <For each={ticks}>{t => 
+            <div class={cn("w-6 border-t-2 border-pink-500 transition-opacity duration-300", (Math.round(offset()/10) === t) ? "opacity-80" : (isHover() || isDragging() ? "opacity-20" : "opacity-10"))}/>
+          }
+          </For>
+        </div>
+
+        <div class="w-2 shadow-inner shadow-black/50 bg-slate-800 rounded flex flex-col items-center mt-2"/>
+
+        <div class="rotate-90 translate-x-5 relative w-0 h-0 overflow-x-visible" draggable={false}>
+          <span class={cn("select-none text-slate-600 absolute", isHover() || isDragging() ? "opacity-100" : "opacity-70" )}>{props.setting.name}</span>
+        </div>
+
+        <div class="absolute flex flex-col items-center" style={{ top: `${offset() * 0.83}%` }}>
+          <div 
+            class={cn(
+              "w-[16px] h-[22px] border-t border-b bg-slate-800 border-slate-600 flex items-center",
+              isDragging() ? "shadow-lg shadow-pink-500/50" : ""
+            )}
+          >
+            <div class={cn("w-full h-0.5 transition-opacity duration-200", isHover() || isDragging() ? "bg-pink-500" : "bg-slate-900" )}/>
+          </div>
+          <div class={cn("mt-1 w-0.5 h-[140px] bg-pink-600 transition-opacity duration-500", (isDragging() || isHover()) ? "opacity-60" : "opacity-30")} />
+        </div>
+        
+      </div>
+      
+      <span class={cn("select-none text-slate-600 transition-opacity duration-200", isHover() || isDragging() ? "opacity-100" : "opacity-70" )}>{Number(props.setting.value).toFixed(1)}</span>
+    </div>
+  )
+}
+
+export default Slider
--- src/shaders/basic.frag
+++ src/shaders/basic.frag
@@ -2,6 +2,9 @@ precision highp float;
 
 const float DATA_LENGTH = 512.0;
 const float ELEMENTS_PER_TEXEL = 3.0;
+const float PI = 3.14159265359;
+const float TWO_PI = 6.28318530718;
+const float HALF_PI = 1.57079632679;
 
 uniform vec2 u_resolution;
 uniform float u_time;
@@ -9,8 +12,9 @@ uniform float u_hue;
 uniform float u_brightness;
 uniform sampler2D u_freq;
 uniform float u_freqScale;
-
-uniform float u_channel;
+uniform float u_freqRange;
+uniform float u_deformation;
+uniform float u_deformationFrequency;
 
 vec3 hsv2rgb(vec3 c) {
   vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
@@ -43,6 +47,7 @@ vec3 hueShift( vec3 color, float hueAdjust ){
     return vec3( dot (yIQ, kYIQToR), dot (yIQ, kYIQToG), dot (yIQ, kYIQToB) );
 }
 
+
 float getComponent(vec4 data, float x) {
   float m = mod(x, ELEMENTS_PER_TEXEL);
   float r = 1.0 - step(1.0, m);
@@ -59,33 +64,52 @@ float noteFrequency(float key) {
   return pow(2.0, (key - 49.0) / 12.0) * 440.0;
 }
 
-void main() {
-  vec2 st = gl_FragCoord.ts / u_resolution.ts;
+float fetchKey(float key, float channel) {
+  float noteFreq = noteFrequency(key);
+  float stx = noteFreq / u_freqScale;
+  vec4 data = texture2D(u_freq, vec2(stx, channel));
+  float x = stx * DATA_LENGTH * ELEMENTS_PER_TEXEL;
+  float amp = getComponent(data, x);
+  return amp;
+}
 
-  float key = st.x * 100.0;
+void main() {
+  vec2 stc = gl_FragCoord.st / u_resolution.tt;
+  stc -= vec2((u_resolution.s / u_resolution.t) * 0.5, 0.5);
+  float r = sqrt(stc.x * stc.x + stc.y * stc.y) * 2.0;
+  float phi = mod(atan(stc.y, stc.x) + HALF_PI, TWO_PI) / TWO_PI;
+
+  float angle = phi > 0.5 ? (1.0 - phi) * 2.0 : phi * 2.0;
+  float key = angle * 88.0 * u_freqRange;
   float noteFreq = noteFrequency(key);
   float stx = noteFreq / u_freqScale;
 
-  vec4 data = texture2D(u_freq, vec2(stx, st.y));
+  float channel = phi > 0.5 ? 0.99 : 0.01;
 
-  float x = stx * DATA_LENGTH * ELEMENTS_PER_TEXEL;
-
-  float amp = getComponent(data, x);
+  r += 
+    (+ sin(phi * TWO_PI * 13.0 + u_time)* fetchKey(13.0 * u_deformationFrequency, 0.0) * 0.02
+    + sin(phi * TWO_PI * 7.0 + u_time)* fetchKey(7.0 * u_deformationFrequency, 0.0) * 0.03
+    + sin(phi * TWO_PI * 5.0 + u_time)* fetchKey(5.0 * u_deformationFrequency, 0.0) * 0.04
+    + sin(phi * TWO_PI * 3.0 + u_time)* fetchKey(3.0 * u_deformationFrequency, 0.0) * 0.04
+    - fetchKey(9.0, 0.0) * 0.03) * u_deformation;
+  
+  float amp = fetchKey(key, channel) + 0.02;
   // freq -= 0.3 * step(0.5, u_channel);
   amp *= log(stx + 2.0);
   amp = exp(amp) - 1.0;
 
-  float distanceFromCenter = abs(st.y - 0.5);
+  float distanceFromCenter = abs(r - 0.4) * (1.0 + step(r - 0.4, 0.0) * 3.0);
   float barHeight = amp * 0.5;
   // float barStrength = timeDomain;
 
-  float a = step(distanceFromCenter, barHeight);
+  float alpha = step(distanceFromCenter, barHeight);
+  float a = alpha;
 
   vec3 innerColor = mix(vec3(0.0078, 0.2196, 0.0941), vec3(0.0863, 0.7098, 0.4902), amp * 2.0 - 0.4);
   vec3 outerColor = mix(vec3(0.0078, 0.0275, 0.0549), vec3(0.251, 0.0275, 0.1216), amp * 2.0 - 0.4);
 
-  innerColor = hueShift(innerColor, -(st.x + u_hue) * 2.);
-  outerColor = hueShift(outerColor, -(st.x + u_hue) * 2.);
+  innerColor = hueShift(innerColor, -(angle + u_hue) * 2.);
+  outerColor = hueShift(outerColor, -(angle + u_hue) * 2.);
 
   vec3 color = mix(innerColor, outerColor, (1.0 - (barHeight - distanceFromCenter) / barHeight));
 
