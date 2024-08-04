eeedf2646761b23b5a566bdec323743bb7324ba9
Veikkosuhonen <vesuvesuh@gmail.com>
2024-08-05 00:20:19 +0300

    Minor betterments

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,29 +21,23 @@ export default function Home() {
           </button>
         </h1>
         <p class="mt-8">
-          This site is mainly for random browser apps I sometimes get an irresistible urge to make, and for some of that portfolio stuff. I might even write a blogpost one day.
+          This place is mainly for random browser apps & experiments I sometimes get an irresistible urge to make.
         </p>
         <h2 class="text-4xl mt-16 font-serif">About me</h2>
         <p class="mt-8">
-          I'm Veikko, a software engineer from Helsinki, Finland with a positive look on life, the universe and everything.
-          Like everybody else, I mostly work in the JavaScript land as a fullstack developer, 
-          enjoying the holistic approach to solving real problems with tech. 
+          I'm Veikko, a software engineer from Helsinki with a positive look on life, the universe and everything.
+          Like everybody else, I mostly work in the JS mines, but I've also experience with Java, Python, Ruby, C++, C# and Rust.
           <br/>
-          <span class="text-xs">I won't bother namedropping the technologies, you can visit my 
+          <span class="text-xs">If you care, you can check out my
           <a href="https://linkedin.com/in/veikko-suhonen-394751230" target="_blank" class="underline text-indigo-300">LinkedIn page <Icon path={arrowTopRightOnSquare} style="width: 12px; display: inline;"/></a></span>
         </p>
         <p class="mt-8">
           I've been studying computer science for <span>{yearsStudied}</span> years, with <span>{yearsProgrammed}</span> years of experience in programming.
           During that time, I've most enjoyed 
-          <span>graphics & game programming</span>, <span>shader development</span>, writing a <a>compiler from scratch</a> and building a bunch of web apps.
-          Some of the stuff I do is public at <a href="https://github.com/Veikkosuhonen" target="_blank" class="underline text-fuchsia-400">github.com/Veikkosuhonen <Icon path={arrowTopRightOnSquare} style="width: 12px; display: inline;"/></a>
+          <span>graphics & game development</span>, <span>shader development</span>, writing a <a>compiler from scratch</a> and building a bunch of web apps.
         </p>
         <p class="mt-8">
-          When I was a kid, I wanted to be a scientist. That hasn't really changed, 
-          I think that the scientific method and quest for new knowledge and truth are the philosophies to live by.
-          <br/>
-          Lately I've been a little bit into astronomy and astrophotography and want to eventually develop my own 
-          <a>lucky-imaging</a> software stack. Stay tuned for any results...
+          Go check out the experiments section for some cool things
         </p>
       </article>
       <EmbersCanvas embersOn={embersOn()} />
