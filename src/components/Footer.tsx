afd9be78f125d76dceffd48abae8e3bc3b685afc
Veikkosuhonen <vesuvesuh@gmail.com>
2024-10-10 02:41:23 +0300

    Add link to vercel depl lol

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 export const Footer = () => {
 
   return (
-    <footer class="z-10 mt-16 p-16 text-slate-600 text-xs font-light">
-      <p class="text-slate-400">This website is made using SolidStart, so its blazingly fast, no question. I'm sure you've noticed already.</p>
-      <a class="text-pink-600 hover:underline" href="https://www.github.com/Veikkosuhonen/veikkosuhonen">github.com/Veikkosuhonen/veikkosuhonen</a>. see you around!
+    <footer class="z-10 mt-16 p-16 flex text-xs text-slate-400">
+      <div>
+        <p>This website is made using SolidStart, so its blazingly fast, no question. I'm sure you've noticed already.</p>
+        <a class="text-sunset-300 hover:underline" href="https://www.github.com/Veikkosuhonen/veikkosuhonen">github.com/Veikkosuhonen/veikkosuhonen</a>. see you around!
+      </div>
+      <a class="text-sunset-300 hover:underline ml-auto" href="https://vercel.com/veikkosuhonens-projects/veikkosuhonen">Vercel deployment</a>, only for ME!
     </footer>
   )
 }
